77d6dc99bcac5273b36ad32e9597ee054b549492
Bert Proesmans <bproesmans@hotmail.com>
2022-12-17 23:49:10 +0100

    Added day01 part 2

--- examples/day01.rs
+++ examples/day01.rs
@@ -1,4 +1,73 @@
+use std::{cell::RefCell, rc::Rc};
+
 //  Get-Content .\examples\day01.stdin | cargo run --example day01
 fn main() {
-    println!("{:?}", aoc2022::day01::most_calories_carrying_elf(std::io::stdin().lines()));
+    let clonable_iterator = std::io::stdin().lines().map(|e| e.map_err(|_| 0));
+    let source = ReplaySubject::new(clonable_iterator);
+    println!(
+        "{:?}",
+        aoc2022::day01::most_calories_carrying_elf(source.clone())
+    );
+    println!(
+        "{:?}",
+        aoc2022::day01::top_n_calories_carrying_elf(source.clone(), 3)
+    );
+}
+
+#[derive(Debug)]
+struct ReplaySubject<IteratorType, ItemType>
+where
+    IteratorType: Iterator<Item = ItemType>,
+    ItemType: Clone,
+{
+    shared_state: Rc<RefCell<(IteratorType, Vec<ItemType>)>>,
+    idx: usize,
+}
+
+impl<IteratorType, ItemType> ReplaySubject<IteratorType, ItemType>
+where
+    IteratorType: Iterator<Item = ItemType>,
+    ItemType: Clone,
+{
+    fn new(iterator: IteratorType) -> Self {
+        Self {
+            shared_state: Rc::new(RefCell::new((iterator, vec![]))),
+            idx: 0,
+        }
+    }
+}
+
+impl<IteratorType, ItemType> Clone for ReplaySubject<IteratorType, ItemType>
+where
+    IteratorType: Iterator<Item = ItemType>,
+    ItemType: Clone,
+{
+    fn clone(&self) -> Self {
+        Self { shared_state: self.shared_state.clone(), idx: self.idx.clone() }
+    }
+}
+
+impl<IteratorType, ItemType> Iterator for ReplaySubject<IteratorType, ItemType>
+where
+    IteratorType: Iterator<Item = ItemType>,
+    ItemType: Clone,
+{
+    type Item = ItemType;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut shared_state = self.shared_state.borrow_mut();
+        let yielded_value = match shared_state.1.get(self.idx) {
+            Some(value) => Some(value.clone()),
+            None => match shared_state.0.next() {
+                Some(next_value) => {
+                    shared_state.1.push(next_value.clone());
+                    Some(next_value.clone())
+                }
+                None => None,
+            },
+        };
+
+        self.idx += 1;
+        yielded_value
+    }
 }
--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,6 @@
-pub fn most_calories_carrying_elf<IteratorType, IntermediateType, ItemType>(it: IteratorType) -> Option<i64>
+pub fn most_calories_carrying_elf<IteratorType, IntermediateType, ItemType>(
+    it: IteratorType,
+) -> Option<i64>
 where
     IteratorType: Iterator<Item = IntermediateType>,
     IntermediateType: IntoIterator<Item = ItemType>,
@@ -15,4 +17,34 @@ where
             Ok(value) => (acc, state + value as i64),
         })
         .0
+}
+
+pub fn top_n_calories_carrying_elf<IteratorType, IntermediateType, ItemType>(
+    it: IteratorType,
+    nth: usize,
+) -> i64
+where
+    IteratorType: Iterator<Item = IntermediateType>,
+    IntermediateType: IntoIterator<Item = ItemType>,
+    ItemType: AsRef<str>,
+{
+    let mut calories: Vec<_> = it
+        .flat_map(|x| x.into_iter().map(|y| y.as_ref().parse::<i32>()))
+        .scan(0, |acc, e| -> Option<Option<i64>> {
+            let mut yield_value = Some(None);
+            match e {
+                Err(_) => {
+                    yield_value = Some(Some(*acc));
+                    *acc = 0;
+                }
+                Ok(value) => *acc += value as i64,
+            };
+            yield_value
+        })
+        // Funky time required because returning a None from iter:scan stops the iterator
+        // So we pad the iterator with None's when values are accumulated
+        .flatten()
+        .collect();
+    calories.sort_unstable_by(|a, b| b.cmp(a)); // sort High -> Low
+    calories.iter().take(nth).sum()
 }
